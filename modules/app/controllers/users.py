609d114db714ba4e90bf2420de77f69f689633a0
yangyuegly <yangyuegly@github.com>
2020-06-25 19:26:12 -0400

    static image

--- modules/app/controllers/users.py
+++ modules/app/controllers/users.py
@@ -4,18 +4,20 @@ import pandas as pd
 from flask import request, jsonify, render_template
 from app import app, listings
 import logger
+import matplotlib.pyplot as plt
+
 import re
+from wordcloud import WordCloud, STOPWORDS
+from .constants import STOPWORDS
 
 ROOT_PATH = os.environ.get('ROOT_PATH')
 LOG = logger.get_root_logger(
     __name__, filename=os.path.join(ROOT_PATH, 'output.log'))
 
 
-@app.route('/country', methods=['POST'])
-def country_price():
-    # country = request.form.to_dict()
-    # all_listing_in_country = listings.find(
-    #     country, projection={"_id": False, "price": True})
+
+@app.route('/data')
+def user():
     all_listing_in_country = listings.aggregate([{
         "$group": {
             "_id": "$address.country",
@@ -24,83 +26,47 @@ def country_price():
             }
         }
     }])
-    all_listing_in_country = list(all_listing_in_country)
+    plt.tight_layout()
+
+    df = pd.DataFrame(list(all_listing_in_country))
+    df.mean_price = df.mean_price.astype('str').astype('float')
+
+    ax = df.plot.bar(x='_id')
+    ax.set_xlabel("Country")
+    ax.figure.savefig('./app/static/pricePlot.png',bbox_inches='tight')
     keys = []
     vals = []
     for c in all_listing_in_country:
-        print(c["_id"])
         keys.append(c["_id"])
         vals.append(c["mean_price"])
     res = dict(zip(keys, vals))
-    return jsonify(res)
+
+    summary = listings.find({"amenities": {"$all": ["Internet"]}})
+
+    summary = list(summary)
+    df = pd.DataFrame(summary)
+    text = df['summary'].tolist()
+    text_string = ' '.join(text)
+    freqMap = getFrequencyDictForText(text_string)
+
+    create_word_cloud(freqMap)
+    return jsonify(freqMap), 200
 
 
-@app.route('/user', methods=['GET', 'POST', 'DELETE', 'PATCH'])
-def user():
-    print("request", request.method)
-    # if request.method == 'GET':
-    #     query = request.args
-    #     print("args", request.args)
-    #     data = listings.find_one(query)
-    #     return jsonify(data), 200
-    # data = request.get_json(force=True)
-    res = ""
-    if request.method == 'POST':
-        query = request.form.to_dict()
-        conditions = []
-        for key in query.keys():
-            conditions.append(key)
-        # query.pop('submitType')
-        print("summary!!!!!:", conditions)
+def create_word_cloud(string):
+    cloud = WordCloud(background_color="black", max_words=1000)
+    cloud.generate_from_frequencies(string)
+    cloud.to_file("./app/static/wordCloud.png")
 
-        summary = listings.find({"amenities": {"$all": conditions}})
 
-        summary = list(summary)
-        # print("result!!!", summary)
-        # summary = listings.find({"ameni": {"$all": ["red", "blank"]}}, limit=200)
-        df = pd.DataFrame(summary)
-        # print('dataf!rame', df['summary'].tolist())
-        text = df['summary'].tolist()
-        # res = []
-        freqMap = {}
-        for item in text:
-            curr = item.split(" ")
-            for c in curr:
-                c = re.sub(r'[^\w]', ' ', c)
-                c = c.lower()
-                if c.isnumeric() or len(c) <= 3:
-                    continue
-                else:
-                    freqMap[c] = freqMap.get(c, 0) + 1
-        freqMap = {k: v for k, v in sorted(
-            freqMap.items(), key=lambda x: x[1], reverse=True)}
-        toRemove = list(freqMap)[400:]
-        for r in toRemove:
-            del freqMap[r]
 
-        for stop in ['with', 'this', 'where', 'there', 'from', 'also', 'just']:
-            del freqMap[stop]
-        print(freqMap)
-        return jsonify(freqMap), 200
-        #     return jsonify({'ok': True, 'message': 'User created successfully!'}), 200
-        # else:
-        #     return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
+def getFrequencyDictForText(sentence):
+    tmpDict = {}
 
-    # if request.method == 'DELETE':
-    #     if data.get('email', None) is not None:
-    #         db_response = listings.delete_one({'email': data['email']})
-    #         if db_response.deleted_count == 1:
-    #             response = {'ok': True, 'message': 'record deleted'}
-    #         else:
-    #             response = {'ok': True, 'message': 'no record found'}
-    #         return jsonify(response), 200
-    #     else:
-    #         return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
+    # making dict for counting frequencies
+    for curr in sentence.split(" "):
+        if not curr in STOPWORDS and re.match("^[a-zA-Z]*$", curr):
+            val = tmpDict.get(curr, 0)
+            tmpDict[curr.lower()] = val + 1
 
-    # if request.method == 'PATCH':
-    #     if data.get('query', {}) != {}:
-    #         listings.update_one(
-    #             data['query'], {'$set': data.get('payload', {})})
-    #         return jsonify({'ok': True, 'message': 'record updated'}), 200
-    #     else:
-    #         return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
+    return tmpDict
